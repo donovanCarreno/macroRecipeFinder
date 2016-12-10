97e2ee63d470b6844c78e8c1b9e5b0f14551d8d0
donovanCarreno <donovan_carreno@yahoo.com>
2016-12-10 14:19:24 -0600

    successful call to api

--- server/server.js
+++ server/server.js
@@ -1,7 +1,42 @@
 const express = require('express')
-
+const https = require('https')
+const bodyParser = require('body-parser')
+const key = ''
 const app = express()
 
+function getRecipes(req, res, next) {
+  const { carbs, fat, protein } = req.query
+  const root = 'spoonacular-recipe-food-nutrition-v1.p.mashape.com';
+	const queryString = `/recipes/findByNutrients?maxcarbs=${carbs}&maxfat=${fat}&maxprotein=${protein}`;
+  const options = {
+    host: root,
+    path: queryString,
+    headers: {
+      'X-Mashape-Key': key
+      // Accept: 'application/json'
+    }
+  }
+
+  https.get(options, (response) => {
+    let body = ''
+
+    response.on('data', (data) => {
+      body += data
+    })
+
+    response.on('end', () => {
+      req.body.recipes = JSON.parse(body)
+      return next()
+    })
+  })
+}
+
 app.use(express.static(__dirname + '/../public'))
+app.use(bodyParser.json())
+
+app.get('/recipes', getRecipes, (req, res) => {
+  console.log(req.body)
+  res.send(req.body.recipes)
+})
 
 module.exports = app
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Input } from './Input'
+import axios from 'axios'
 
 export default class Nav extends React.Component {
   constructor() {
     super()
     this.getNutrientValue = this.getNutrientValue.bind(this)
+    this.handleClick = this.handleClick.bind(this)
     this.state = {
       Protein: 0,
       Carbs: 0,
@@ -18,12 +20,24 @@ export default class Nav extends React.Component {
     })
   }
 
+  handleClick(e) {
+    e.preventDefault()
+    const { Protein, Carbs, Fat } = this.state
+    e.target.reset()
+    // GET /shoes?order=desc&shoe[color]=blue&shoe[type]=converse
+    axios.get(`/recipes?protein=${Protein}&carbs=${Carbs}&fat=${Fat}`)
+      .then(res => console.log(res))
+  }
+
   render() {
     return (
       <div>
-        <Input nutrient='Protein' getNutrientValue={this.getNutrientValue} />
-        <Input nutrient='Carbs' getNutrientValue={this.getNutrientValue} />
-        <Input nutrient='Fat' getNutrientValue={this.getNutrientValue} />
+        <form onSubmit={this.handleClick}>
+          <Input nutrient='Protein' getNutrientValue={this.getNutrientValue} />
+          <Input nutrient='Carbs' getNutrientValue={this.getNutrientValue} />
+          <Input nutrient='Fat' getNutrientValue={this.getNutrientValue} />
+          <button>Search</button>
+        </form>
       </div>
     )
   }
